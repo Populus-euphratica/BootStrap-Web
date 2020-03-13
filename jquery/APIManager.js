748544b7b892f28754c48a2c93d5d8f24a25bcfe
Populus-euphratica <18832926135@163.com>
2020-03-13 20:17:02 +0800

    完成管理员界面导航栏

--- jquery/APIManager.js
+++ jquery/APIManager.js
@@ -7,6 +7,21 @@ var searchUrl = "";
 var searchType = "";
 var searchValue = "";
 var data1 = {};
+
+var stateData=JSON.parse(sessionStorage.getItem("stateData"));
+$(document).ready(init());
+function init(){
+  
+  var login=sessionStorage.getItem("login");
+  if(login!="true"||typeof(stateData) == "undefined"||$.isEmptyObject(stateData)){
+    window.location.href="Login.html";
+  }
+  
+  $("#state").html("<i class='fa fa-user fa-fw' aria-hidden='true'></i>"+stateData.name);
+  
+  
+}
+
 // 实现搜索功能
 function search() {
 
